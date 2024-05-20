caa4a4c2d251c054cb3082ac12a0853691fbb49d
SDHINSA <sohanad@icloud.com>
2024-05-20 12:36:42 -0600

    Done step 3

--- eas.js
+++ eas.js
@@ -17,3 +17,29 @@ for(let i = 0; i <= 15; i++){
 // use a double for loop and in the first one make the row it belongs to and the second one add the box 
 // create a div and then append it to the main one in js 
 
+
+
+const gc = document.querySelectorAll('.grid');
+
+gc.forEach(cell => {
+
+    cell.addEventListener('mouseover', ()=>{
+        cell.style.backgroundColor = 'blue';
+    });
+    cell.addEventListener('mouseout', () => {
+        setTimeout(() => {
+            cell.style.backgroundColor='';
+        },500);
+    });
+});
+
+
+
+
+
+//Hover Effect
+//1. get the element(s)
+//2. add event listener mouse over 
+// - change the colour 
+//3. add event listener mouse under 
+//- back to og colour 
