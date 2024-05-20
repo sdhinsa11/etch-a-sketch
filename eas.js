a69163da343982e652e453ef6beec63abee91c23
SDHINSA <sohanad@icloud.com>
2024-05-20 12:40:22 -0600

    Added a colouring scheme

--- eas.js
+++ eas.js
@@ -18,13 +18,35 @@ for(let i = 0; i <= 15; i++){
 // create a div and then append it to the main one in js 
 
 
+//Hover Effect
+//1. get the element(s)
+//2. add event listener mouse over 
+// - change the colour 
+//3. add event listener mouse under 
+//- back to og colour 
+
+//Generate random colours for each grid 
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+function getCharacter(index) {
+	return hexCharacters[index]
+}
+function generateNewColor() {
+	let hexColorRep = "#"
+
+	for (let index = 0; index < 6; index++){
+		const randomPosition = Math.floor ( Math.random() * hexCharacters.length ) 
+    	hexColorRep += getCharacter( randomPosition )
+	}
+	return hexColorRep
+}
+
+
 
 const gc = document.querySelectorAll('.grid');
-
 gc.forEach(cell => {
 
     cell.addEventListener('mouseover', ()=>{
-        cell.style.backgroundColor = 'blue';
+        cell.style.backgroundColor = generateNewColor();
     });
     cell.addEventListener('mouseout', () => {
         setTimeout(() => {
@@ -36,10 +58,3 @@ gc.forEach(cell => {
 
 
 
-
-//Hover Effect
-//1. get the element(s)
-//2. add event listener mouse over 
-// - change the colour 
-//3. add event listener mouse under 
-//- back to og colour 
